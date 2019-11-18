25c3d6787630163b59d1cd40fbaca9cdb2468a28
Ottoniel Matheus <ottoniel.matheus@gmail.com>
2019-11-17 23:18:09 -0300

    povoate updated.

--- PovoateFluxControl.sql
+++ PovoateFluxControl.sql
@@ -9,15 +9,13 @@ INSERT INTO UserTypes
 VALUES ('Gerente');
 INSERT INTO UserTypes
 VALUES ('Administrador');
-
-select * from UserTypes
+select * from UserTypes;
 
 INSERT INTO Users 
-VALUES ('Ottoniel Matheus', 100419, 'ottonielrp2008@gmail.com', '123456', 3, GETDATE(), NULL),
+VALUES ('Ottoniel Matheus', 100419, 'ottonielrp2008@gmail.com', '123456', 4, GETDATE(), NULL),
 ('Caio Santanna', 101419, 'caiorsantanna@gmail.com', '123456', 2, GETDATE(), NULL),
 ('Gabriel Borges', 102419, 'borges@gmail.com', '123456', 3, GETDATE(), NULL);
-
-select * from Users
+select * from Users;
 
 INSERT INTO Companies
 VALUES ('Itamarati', '/url/image1.png', 30, GETDATE(), NULL)
@@ -26,6 +24,7 @@ VALUES ('Cometa', '/url/image2.png', 7, GETDATE(), NULL)
 INSERT INTO Companies
 VALUES ('Santa Luzia', '/url/image3.png', 15, GETDATE(), NULL)
 GO
+select * from Companies;
 
 INSERT INTO Buses
 VALUES (27320, 'ABC-1234', GETDATE(), NULL, 1)
@@ -41,19 +40,14 @@ VALUES (27331, 'LMN-1234', GETDATE(), NULL, 2)
 
 INSERT INTO Buses
 VALUES (27342, 'OPQ-1234', GETDATE(), NULL, 3)
-
-SELECT company.*, bus.Id Bus_Id, bus.Number, bus.LicensePlate, bus.Company_Id
-FROM Companies company
-JOIN Buses bus ON bus.Company_Id = company.Id
-ORDER BY Company_Id
-
 SELECT * FROM Buses;
-SELECT * FROM Users;
 
 sp_help FlowRecords;
-
 SELECT * FROM FlowRecords
 
 INSERT INTO ProviderRules (IntervalMinutes, Tax) VALUES (1, 50);
 
+sp_help ProviderRules;
 select * from ProviderRules;
+
+select * from Invoices
